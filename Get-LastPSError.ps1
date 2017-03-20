640f0cb61658283f47bac5322d0c9f74da934556
Joshua Barton <FooBartn@users.noreply.github.com>
2017-03-20 00:10:29 -0500

    Update Get-LastPSError.ps1

--- Get-LastPSError.ps1
+++ Get-LastPSError.ps1
@@ -1,12 +1,24 @@
 $LastLog = 'C:\\Program Files (x86)\\Cisco Systems\\Cisco PSA Service\\logs\\log.txt'
 $ErrorString = 'ERROR processing URL Exception while executing script'
 $EndLine = 'URL Processing completed'
-$LogContent = Get-Content $LastLog
-$LastError = ($LogContent |
+
+try {
+    $LogContent = Get-Content $LastLog
+} catch {
+    Write-Output "Couldn't retrieve content from $LastLog"
+    Exit 1
+}
+
+$StartingLine = ($LogContent |
     Select-String $ErrorString |
     Select-Object -First 1).LineNumber - 1
 
+$ErrorLine = $StartingLine
+
 do {
-    Write-Output "$($LogContent[$LastError])"
-    $LastError++
-} until ($LogContent[$LastError] -like "*$EndLine")
+    Write-Output "$($LogContent[$ErrorLine])"
+    $ErrorLine++
+} until (
+    $LogContent[$ErrorLine] -like "*$EndLine" -or
+    ($ErrorLine - $StartingLine) -gt 100
+)
